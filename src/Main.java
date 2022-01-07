6f6bee05406cae9487f7e04c3dab3dc106e7fd9c
TerminalJunky <bryan@terminaljunky.com>
2022-01-06 18:55:15 -0700

    build number update
    forgot to change build number date

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Main {
 	public static void main(String args[])
 	{
-		String build_number = "20220102";
+		String build_number = "20220106";
 		storage s = new storage();
 		s.start(build_number);
 		s.recursive_start(s, build_number);
